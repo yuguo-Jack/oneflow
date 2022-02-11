048363e9c188ada85e4efbeebf84410f5c43025e
Aries-chen <1416535234@qq.com>
2022-02-11 10:45:14 +0800

    simplify the description

--- flowvision/layers/attention/se.py
+++ flowvision/layers/attention/se.py
@@ -11,8 +11,8 @@ class SEModule(nn.Module):
         channels (int): The input channel size
         reduction (int): Ratio that allows us tovary the capacity and computational cost of the SE model. Default: 16
         rd_channels (int or None): Number of reduced channels. If none, uses reduction to calculate
-        act_layer (flow.nn.Module): An activation layer used between two FC layers of Excitation. Default: flow.nn.ReLU
-        gate_layer (flow.nn.Module): An activation layer used after two FC layers of Excitation. Default: flow.nn.Sigmoid
+        act_layer (flow.nn.Module): An activation layer used between two FC layers. Default: flow.nn.ReLU
+        gate_layer (flow.nn.Module): An activation layer used after two FC layers. Default: flow.nn.Sigmoid
         mlp_bias (bool): If True, add learnable bias to the linear layers. Default: False
     """
 
