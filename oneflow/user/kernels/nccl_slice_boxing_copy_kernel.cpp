2f9750a6ad1a5f404475075d133c7b84c1e00e44
guo-ran <360112263@qq.com>
2022-03-29 19:56:00 +0800

    support B

--- oneflow/user/kernels/nccl_slice_boxing_copy_kernel.cpp
+++ oneflow/user/kernels/nccl_slice_boxing_copy_kernel.cpp
@@ -22,10 +22,54 @@ limitations under the License.
 #include "oneflow/core/register/tensor_slice_copier.h"
 #include "oneflow/user/ops/nccl_logical_util.h"
 #include "oneflow/core/job/nd_sbp_util.h"
+#include "oneflow/core/common/nd_index_offset_helper.h"
 
 namespace oneflow {
 
 namespace {
+
+// Go through all the ranks while transfer between two nd sbps with no PartialSum under the same
+// placement.
+// NOTE: We need to make sure no partial sums in the sbps of the producer and consumer.
+void DfsTraverseRanks4NdSbp(
+    int32_t depth, std::vector<int64_t>& in_parallel_ids,
+    const std::vector<int64_t>& out_parallel_ids, const Shape& parallel_hierarchy,
+    const NdIndexOffsetHelper<int64_t, SHAPE_MAX_AXIS_SIZE>& hierarchy_index_helper,
+    const NdSbp& in_nd_sbp, const std::function<void(int32_t, int32_t)>& visit) {
+  if (depth >= parallel_hierarchy.NumAxes()) {
+    visit(hierarchy_index_helper.NdIndexToOffset(out_parallel_ids.data(),
+                                                 parallel_hierarchy.NumAxes()),
+          hierarchy_index_helper.NdIndexToOffset(in_parallel_ids.data(),
+                                                 parallel_hierarchy.NumAxes()));
+    return;
+  }
+  if (in_nd_sbp.sbp_parallel(depth).has_broadcast_parallel()) {
+    // If Broadcast in the sbp of the producer, only visit those ranks with the same id as the
+    // current rank along the depth-dimension.
+    in_parallel_ids[depth] = out_parallel_ids[depth];
+    DfsTraverseRanks4NdSbp(depth + 1, in_parallel_ids, out_parallel_ids, parallel_hierarchy,
+                           hierarchy_index_helper, in_nd_sbp, visit);
+  } else {
+    // If Split or PartialSum, go through all the ranks along the depth-dimension.
+    for (int64_t i = 0; i < parallel_hierarchy.dim_vec().at(depth); i++) {
+      in_parallel_ids[depth] = i;
+      DfsTraverseRanks4NdSbp(depth + 1, in_parallel_ids, out_parallel_ids, parallel_hierarchy,
+                             hierarchy_index_helper, in_nd_sbp, visit);
+    }
+  }
+}
+
+void DfsTraverse4NdSbp(int64_t out_id, const std::shared_ptr<Shape> parallel_hierarchy,
+                       const NdSbp& in_nd_sbp, const std::function<void(int32_t, int32_t)>& visit) {
+  int32_t hierarchy_dimension = parallel_hierarchy->NumAxes();
+  const NdIndexOffsetHelper<int64_t, SHAPE_MAX_AXIS_SIZE> hierarchy_index_helper(
+      parallel_hierarchy->dim_vec().data(), hierarchy_dimension);
+  std::vector<int64_t> in_parallel_ids(hierarchy_dimension);
+  std::vector<int64_t> out_parallel_ids(hierarchy_dimension);
+  hierarchy_index_helper.OffsetToNdIndex(out_id, out_parallel_ids.data(), hierarchy_dimension);
+  DfsTraverseRanks4NdSbp(0, in_parallel_ids, out_parallel_ids, *parallel_hierarchy,
+                         hierarchy_index_helper, in_nd_sbp, visit);
+}
 
 class NcclSliceBoxingKernelState final : public user_op::OpKernelState {
  public:
@@ -42,8 +86,9 @@ class NcclSliceBoxingKernelState final : public user_op::OpKernelState {
     CHECK_JUST(GetNcclLogicalNdSbpFromAttr(ctx, "src_nd_sbp", &src_nd_sbp));
     NdSbp dst_nd_sbp;
     CHECK_JUST(GetNcclLogicalNdSbpFromAttr(ctx, "dst_nd_sbp", &dst_nd_sbp));
-    CHECK_EQ(src_nd_sbp.sbp_parallel_size(), parallel_desc_.hierarchy()->NumAxes());
-    CHECK_EQ(dst_nd_sbp.sbp_parallel_size(), parallel_desc_.hierarchy()->NumAxes());
+    const auto& parallel_hierarchy = parallel_desc_.hierarchy();
+    CHECK_EQ(src_nd_sbp.sbp_parallel_size(), parallel_hierarchy->NumAxes());
+    CHECK_EQ(dst_nd_sbp.sbp_parallel_size(), parallel_hierarchy->NumAxes());
     const user_op::TensorDesc* in_logical_desc = ctx->LogicalTensorDesc4ArgNameAndIndex("in", 0);
     const DataType data_type = in_logical_desc->data_type();
     const DeviceType device_type = parallel_desc_.device_type();
@@ -53,28 +98,37 @@ class NcclSliceBoxingKernelState final : public user_op::OpKernelState {
         GetTensorSliceView(*parallel_desc_.hierarchy(), dst_nd_sbp, logical_shape);
     const std::vector<TensorSliceView>& in_slices =
         GetTensorSliceView(*parallel_desc_.hierarchy(), src_nd_sbp, logical_shape);
-    for (int64_t i = 0; i < parallel_num; ++i) {
-      const TensorSliceView& cur_rank_in_slice = in_slices.at(parallel_id_);
-      const TensorSliceView& in_intersection = out_slices.at(i).Intersect(cur_rank_in_slice);
-      if (!in_intersection.IsEmpty()) {
-        send_elem_cnts_.push_back(in_intersection.shape().elem_cnt());
-        in_tensor_slice_copier_vec_.emplace_back(
-            new TensorSliceCopier(in_intersection, cur_rank_in_slice, data_type, device_type));
-      } else {
-        send_elem_cnts_.push_back(0);
-        in_tensor_slice_copier_vec_.emplace_back(nullptr);
-      }
 
-      const TensorSliceView& cur_rank_out_slice = out_slices.at(parallel_id_);
-      const TensorSliceView& out_interaction = cur_rank_out_slice.Intersect(in_slices.at(i));
-      if (!out_interaction.IsEmpty()) {
-        recv_elem_cnts_.push_back(out_interaction.shape().elem_cnt());
-        out_tensor_slice_copier_vec_.emplace_back(
-            new TensorSliceCopier(cur_rank_out_slice, out_interaction, data_type, device_type));
-      } else {
-        recv_elem_cnts_.push_back(0);
-        out_tensor_slice_copier_vec_.emplace_back(nullptr);
-      }
+    // add to cur_out_slice
+    recv_elem_cnts_.resize(parallel_num);
+    out_tensor_slice_copier_vec_.resize(parallel_num);
+    const TensorSliceView& cur_rank_out_slice = out_slices.at(parallel_id_);
+    const auto& add_to_out_slice_vec = [&](int32_t out_id, int32_t in_id) {
+      CHECK_EQ(out_id, parallel_id_);
+      const TensorSliceView& in_slice = in_slices.at(in_id);
+      const TensorSliceView& intersection = cur_rank_out_slice.Intersect(in_slice);
+      if (intersection.IsEmpty()) { return; }
+      recv_elem_cnts_.at(in_id) = intersection.shape().elem_cnt();
+      out_tensor_slice_copier_vec_.at(in_id).reset(
+          new TensorSliceCopier(cur_rank_out_slice, intersection, data_type, device_type));
+    };
+    DfsTraverse4NdSbp(parallel_id_, parallel_hierarchy, src_nd_sbp, add_to_out_slice_vec);
+
+    // add to cur_in_slice
+    send_elem_cnts_.resize(parallel_num);
+    in_tensor_slice_copier_vec_.resize(parallel_num);
+    const TensorSliceView& cur_rank_in_slice = in_slices.at(parallel_id_);
+    const auto& add_to_in_slice_vec = [&](int32_t out_id, int32_t in_id) {
+      if (in_id != parallel_id_) { return; }
+      const TensorSliceView& out_slice = out_slices.at(out_id);
+      const TensorSliceView& intersection = out_slice.Intersect(cur_rank_in_slice);
+      if (intersection.IsEmpty()) { return; }
+      send_elem_cnts_.at(out_id) = intersection.shape().elem_cnt();
+      in_tensor_slice_copier_vec_.at(out_id).reset(
+          new TensorSliceCopier(intersection, cur_rank_in_slice, data_type, device_type));
+    };
+    for (int64_t i = 0; i < parallel_num; ++i) {
+      DfsTraverse4NdSbp(i, parallel_hierarchy, src_nd_sbp, add_to_in_slice_vec);
     }
   }
   ~NcclSliceBoxingKernelState() {}
@@ -177,7 +231,7 @@ class NcclSliceBoxingCopyKernel final : public user_op::OpKernel {
     const std::vector<std::shared_ptr<TensorSliceCopier>>& in_tensor_slice_copier_vec =
         kernel_state->in_tensor_slice_copier_vec();
     for (int64_t i = 0; i < parallel_num; ++i) {
-      if (in_tensor_slice_copier_vec.at(i) != nullptr) {
+      if (in_tensor_slice_copier_vec.at(i)) {
         in_tensor_slice_copier_vec.at(i)->Copy(ctx->stream(), send_in_ptr.at(i), in->dptr());
       }
     }
@@ -221,10 +275,11 @@ class NcclSliceBoxingCopyKernel final : public user_op::OpKernel {
     const std::vector<std::shared_ptr<TensorSliceCopier>>& out_tensor_slice_copier_vec =
         kernel_state->out_tensor_slice_copier_vec();
     for (int64_t i = 0; i < parallel_num; ++i) {
-      if (out_tensor_slice_copier_vec.at(i) != nullptr) {
+      if (out_tensor_slice_copier_vec.at(i)) {
         out_tensor_slice_copier_vec.at(i)->Copy(ctx->stream(), out->mut_dptr(), recv_out_ptr.at(i));
       }
     }
+    // TODO :support in has partialsum
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
