824202e087f15d8f5d901249c03f55de935ec160
Ldpe2G <liangdepeng@gmail.com>
2022-03-30 10:33:04 +0800

    add einsum profile script

---
+++ python/oneflow/test/modules/profile_einsum/einsum_speed_test.py
@@ -0,0 +1,151 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow as flow
+import torch
+
+import time
+import random
+import argparse
+
+
+def parse_option():
+    parser = argparse.ArgumentParser("einsum speed test", add_help=False)
+    parser.add_argument("--warm_up_iters", type=int, default=5, help="warm up iters")
+    parser.add_argument("--run_iters", type=int, default=100, help="cal time iters")
+    parser.add_argument(
+        "--min_size_for_each_dim", type=int, default=100, help="random size range"
+    )
+    parser.add_argument(
+        "--max_size_for_each_dim", type=int, default=200, help="random size range"
+    )
+    parser.add_argument("--einsum_testcase", type=str, help="random size range")
+    parser.add_argument("--ellipsis_dims", type=int, default=2, help="cal time iters")
+
+    args, unparsed = parser.parse_known_args()
+
+    return args
+
+
+args = parse_option()
+
+
+usecase_to_equations = {
+    "matrix_transpose": "ij->ji",
+    "eltwise_mul": "ij,ij->ij",
+    "get_diagonal": "ii->i",
+    "batch_permute": "...ij->...ji",
+    "reduce_sum": "ij->",
+    "matrix_column_sum": "ij->j",
+    "matrix_vector_multiply": "ik,k->i",
+    "matmul": "ik,kj->ij",
+    "vector_inner_product": "i,i->",
+    "eltwise_mul_then_reduce_sum": "ij,ij->",
+    "vector_outer_product": "i,j->ij",
+    "batch_matmul": "ijk,ikl->ijl",
+    "tensor_contraction": "pqrs,tuqvr->pstuv",
+    "bilinear_transformation": "ik,jkl,il->ij",
+    "attention": "bhid,bhjd->bhij",
+    "batch_matmul2": "bhij,bhjd->bhid",
+    "batch_matmul3": "bxid,bjd->bxij",
+    "batch_matmul4": "bxij,bjd->bxid",
+    "batch_matrix_vector_multiply": "bid,bijd->bij",
+    "alphaflod_usecase1": "hij,ijc->ihc",
+    "alphaflod_usecase2": "rac,rab->rbc",
+    "alphaflod_usecase3": "ra,rab->rb",
+    "alphaflod_usecase4": "qhc,khc->qkh",
+    "alphaflod_usecase5": "nm,mrc->nrc",
+    "alphaflod_usecase6": "abc,adc->bdc",
+    "alphaflod_usecase7": "dceb,cef->dbf",
+    "alphaflod_usecase8": "acb,ade->dceb",
+    "alphaflod_usecase9": "qkc,ch->hqk",
+    "alphaflod_usecase10": "bhqk,bkhc->bqhc",
+    "alphaflod_usecase11": "bqa,ahc->bqhc",
+}
+
+if args.einsum_testcase not in usecase_to_equations:
+    print("not supported test case.")
+    print(f"please choose cases in: {list(usecase_to_equations.keys())}")
+    exit(0)
+
+equation = usecase_to_equations[args.einsum_testcase]
+
+
+def get_input_tensors_shapes(equation):
+    input_eqs = equation.split("->")[0].split(",")
+    shapes = []
+    meet = {}
+    for eq in input_eqs:
+        shape = []
+        # deal with batch_permute, not general
+        if "..." in eq:
+            eq = eq.split("...")[1]
+            for d in range(args.ellipsis_dims):
+                shape.append(
+                    random.randint(
+                        args.min_size_for_each_dim, args.max_size_for_each_dim
+                    )
+                )
+
+        for s in eq:
+            if s in meet:
+                shape.append(meet[s])
+            else:
+                random_shape = random.randint(
+                    args.min_size_for_each_dim, args.max_size_for_each_dim
+                )
+                shape.append(random_shape)
+                meet[s] = random_shape
+        shapes.append(shape)
+    return shapes
+
+
+input_shapes = get_input_tensors_shapes(equation)
+
+print(f"\n# {args.einsum_testcase}")
+print(f'einsum equation: "{equation}"')
+shapes_str = ", ".join([f"{s}" for s in input_shapes])
+print(f"input tensor shapes: {shapes_str}")
+
+# oneflow
+input_tensors = [flow.rand(*shape, device="cuda") for shape in input_shapes]
+for i in range(args.warm_up_iters + args.run_iters):
+    if i == args.warm_up_iters:
+        start_time = time.time()
+    out = flow.einsum(equation, *input_tensors)
+    out_np = out.numpy()
+total_time_ms = (time.time() - start_time) * 1000
+oneflow_time_per_run_ms = total_time_ms / args.run_iters
+print(
+    f"oneflow avg time per iter: {oneflow_time_per_run_ms:.2f} ms (= {total_time_ms:.1f}ms / {args.run_iters})"
+)
+
+# pytorch
+torch_input_tensors = [torch.rand(*shape, device="cuda") for shape in input_shapes]
+for i in range(args.warm_up_iters + args.run_iters):
+    if i == args.warm_up_iters:
+        start_time = time.time()
+    out = torch.einsum(equation, *torch_input_tensors)
+    out_np = out.cpu().numpy()
+total_time_ms = (time.time() - start_time) * 1000
+torch_time_per_run_ms = total_time_ms / args.run_iters
+print(
+    f"pytorch avg time per iter: {torch_time_per_run_ms:.2f} ms (= {total_time_ms:.1f}ms / {args.run_iters})"
+)
+
+relative_speed = torch_time_per_run_ms / oneflow_time_per_run_ms
+print(
+    f"Relative speed: {relative_speed:.2f} (= {torch_time_per_run_ms:.2f}ms / {oneflow_time_per_run_ms:.2f}ms)\n"
+)
