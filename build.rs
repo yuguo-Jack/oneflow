611febe29de73b0bbf1e647a3f34691b092057c2
jackalcooper <jackalcooper@gmail.com>
2022-04-13 02:04:55 +0000

    fix

--- build.rs
+++ build.rs
@@ -1,10 +1,14 @@
 // build.rs
 
+use std::{env, path::PathBuf};
+
 use glob::glob;
 
 fn main() {
     let glog_url = "https://github.com/google/glog/archive/refs/tags/v0.5.0.tar.gz";
     let glog_hash = "2368e3e0a95cce8b5b35a133271b480f";
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let out_include: PathBuf = [out_dir.to_str().unwrap(), "include"].iter().collect();
     let dst = cmake::Config::new("external/stub")
         .define("GLOG_URL", glog_url)
         .define("GLOG_HASH", glog_hash)
@@ -19,6 +23,9 @@ fn main() {
             Err(e) => println!("{:?}", e),
         }
     }
-    oneflow_common.include(".").compile("oneflow_common");
+    oneflow_common
+        .include(".")
+        .include(out_include)
+        .compile("oneflow_common");
     println!("cargo:rerun-if-changed=src/hello.c");
 }
