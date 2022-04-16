bc57cf8ebd33b79751ee900f33bd251004824b74
luqiang-guo <702572275@qq.com>
2022-04-16 05:50:36 +0000

    Revert "fix"
    This reverts commit 37608e3daa623397bc932fcdb8b19bf8c27cb205.

--- oneflow/api/python/functional/python_arg_parser.cpp
+++ oneflow/api/python/functional/python_arg_parser.cpp
@@ -77,8 +77,12 @@ bool FunctionSchema::Parse(PyObject* args, PyObject* kwargs, PythonArg* parsed_a
       }
       obj = PyTuple_GetItem(args, arg_pos);
     } else if (kwargs) {
-      obj = PyDict_GetItemString(kwargs, param.name.c_str());
-      if (obj) { remaining_kwargs--; }
+      PyObject* key = PyBytes_FromString(param.name.c_str());
+      if (PyDict_Contains(kwargs, key)) {
+        obj = PyDict_GetItem(kwargs, key);
+        remaining_kwargs--;
+      }
+      Py_DECREF(key);
     }
 
     if (obj) {
