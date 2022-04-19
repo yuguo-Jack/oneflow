70e81efd1d0ae9ecf522712377901679e3feffee
jackalcooper <jackalcooper@gmail.com>
2022-04-19 10:24:10 +0800

    refine

--- ci/earthly/cu102.sh
+++ ci/earthly/cu102.sh
@@ -2,4 +2,5 @@ set -e
 earthly \
     --build-arg BASE_IMAGE=registry.cn-beijing.aliyuncs.com/oneflow/manylinux2014_x86_64_cuda10.2 \
     --build-arg ONEFLOW_CI_CMAKE_INIT_CACHE=cmake/caches/ci/release/cuda.cmake \
+    --build-arg ONEFLOW_CI_BUILD_PARALLEL=16 \
     +build
